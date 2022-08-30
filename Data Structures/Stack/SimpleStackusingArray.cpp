53ad82a95f596abcdfe64dd707fdf80a1c927306
Code_Blooded_17 <70679822+PranshuKala@users.noreply.github.com>
2022-08-30 17:25:57 +0530

    Create SimpleStackusingArray.cpp

---
+++ Data Structures/Stack/SimpleStackusingArray.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+int top = -1;
+void push(int arr[], int s)
+{
+    int pos = top + 1;
+    if (top + 1 >= s)
+    {
+        cout << "Stack Overflow \n";
+    }
+    else
+    {
+        cout << "Enter value to be inserted :";
+        cin >> arr[pos];
+        cout << "Element inserted ! \n";
+        top = top + 1;
+    }
+}
+void display(int arr[])
+{
+    int i = top;
+    if (top == -1)
+    {
+        cout << "The stack is empty \n";
+    }
+    else
+    {
+        while (i >= 0)
+        {
+            cout << "|" << arr[i] << "|"
+                 << "\n";
+            i--;
+        }
+    }
+}
+void pop(int arr[])
+{
+    if (top == -1)
+    {
+        cout << "The stack is empty \n";
+    }
+    else
+    {
+        cout << "The popped element is :" << arr[top] << "\n";
+        top = top - 1;
+    }
+}
+void count(int arr[])
+{
+    int i = top;
+    if (top == -1)
+    {
+        cout << "The stack is empty \n";
+    }
+    else
+    {
+        cout << "The total elements in the stack are:" << top + 1 << "\n";
+    }
+}
+
+int main()
+{
+    cout << "Enter the size of stack :";
+    int s, c;
+    cin >> s;
+    int stack[s];
+    cout << "Stack created ! \n";
+    do
+    {
+        cout << "Enter your choice: \n";
+        cout << "0 Exit \n";
+        cout << "1 Push Value \n";
+        cout << "2 Pop value \n";
+        cout << "3 Count No of elements \n";
+        cout << "4 Display Elements \n";
+        cin >> c;
+        switch (c)
+        {
+        case 1:
+        {
+            push(stack, s);
+            continue;
+        }
+        case 2:
+        {
+            pop(stack);
+            continue;
+        }
+        case 3:
+        {
+            count(stack);
+            continue;
+        }
+        case 4:
+        {
+            display(stack);
+            continue;
+        }
+        }
+
+    } while (c != 0);
+}
