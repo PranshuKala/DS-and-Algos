1072dc2df8ad48568b216aea882b49315c937541
Code_Blooded_17 <70679822+PranshuKala@users.noreply.github.com>
2022-11-13 12:42:52 +0530

    Update InfixtoPostfix.cpp

--- Algorithms/Stack/InfixtoPostfix.cpp
+++ Algorithms/Stack/InfixtoPostfix.cpp
@@ -1,64 +1,62 @@
-//works only for expression containing *,-,+,/
-
 #include <iostream>
 #include <stack>
 #include <string>
 using namespace std;
-int priority(char c)
-{
+
+
+int priority(char c){
     int p;
-    if (c == '+' || c == '-')
-    {
-        p = 1;
+    if (c == '+' || c == '-'){ p = 1;
     }
-    else if (c == '*' || c == '/')
-    {
-        p = 2;
+    else if (c == '*' || c == '/'){ p = 2;
+    }
+    else if (c == '^'){ p = 3;
+    }
+    else { p = 0;
     }
     return p;
 }
 
-int checkprecedence(char a, char b)
-{
+int checkprecedence(char a, char b){
     int p1 = priority(a);
     int p2 = priority(b);
-    if (p1 > p2)
-    {
-        return true;
+    if (p1 > p2){ return true;
     }
-    else if (p1 == p2)
-    {
-        return true;
+    else if (p1 == p2){ return true;
     }
 
-    else
-    {
-        return false;
+    else{ return false;
     }
 }
 int main()
 {
-    string input, output;
+    string s, output;
     stack<char> temp;
-    cout << "Enter the expression (=,-,/,*): ";
-    cin >> input;
-    int len = input.length();
-    for (int i = 0; i < len; i++)
-    {
-        if (input[i] == '+' || input[i] == '-' ||
-            input[i] == '*' || input[i] == '/')
+    cout << "Enter the expression (=,-,/,*) : ";
+    cin >> s;
+    for (int i = 0; i < s.length(); i++){
+        if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i]=='^')
         {
-            while (!temp.empty() &&
-                   checkprecedence(temp.top(), input[i]))
+          while (!temp.empty() && checkprecedence(temp.top(), s[i]))
             {
                 output = output + temp.top();
                 temp.pop();
             }
-            temp.push(input[i]);
+            temp.push(s[i]);
         }
-        else
-        {
-            output = output + input[i];
+        else if(s[i] == '('){
+            temp.push(s[i]);
+        }
+
+        else if(s[i] == ')'){
+            while(temp.top() != '('){
+                output = output + temp.top();
+                temp.pop();
+            }
+            temp.pop();
+        }
+        else{
+            output = output + s[i];
         }
     }
     while (temp.empty() != true)
